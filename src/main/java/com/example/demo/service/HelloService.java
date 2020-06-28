9d31639f434a2551d7ec09d721b16a915692157a
vn50ggs <Van.Ha.Nguyen@walmart.com>
2020-06-28 13:35:35 -0500

    implemented writing the current date time to fine

--- src/main/java/com/example/demo/service/HelloService.java
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -2,17 +2,30 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Service
 public class HelloService {
 
-    public void saveFile(){
+    private static final String OUTPUT_FILE = "/var/log/requests.txt";
+
+    public void saveFile() {
         ZoneId zoneId = ZoneId.of("America/Chicago");
         ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
-        System.out.println(zonedDateTime);
+        writeToFile(zonedDateTime + "\n");
     }
 
+    private static void writeToFile(String content) {
+        try {
+            FileWriter fw = new FileWriter(OUTPUT_FILE, true); //the true will append the new data
+            fw.write(content);//appends the string to the file
+            fw.close();
+        } catch (IOException ioe) {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
+    }
 
 }
